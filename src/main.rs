a3971e02fa2c6520624fe28dd0843e598caf8483
Kdwk <kdwkleung@proton.me>
2023-06-04 19:57:57 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::{Router, response::IntoResponse, routing::get};
+use std::net::SocketAddr;
+
+// #[tokio::main]
+#[shuttle_runtime::main]
+async fn shuttle() -> shuttle_axum::ShuttleAxum {
+    let app = Router::new().route("/", get(hello_world).into());
+    Ok(app.into())
+    // let addr = SocketAddr::new([0,0,0,0].into(), 3000);
+    // axum::Server::bind(&addr)
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
+}
+
+async fn hello_world() -> impl IntoResponse { // allow return of anything that can be turned into a response (anything that implements IntoResponse trait)
+    "Hello World"
+}
+
+async fn test() -> impl IntoResponse {
+
+}
